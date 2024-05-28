555e1320b39df6480e61a34f6f8ec14f6353c32f
yogeshwaran <yogeshwaran.mohan@ideas2it.com>
2024-05-28 16:57:41 +0530

    Fix : Implemented the Loyalty and reduced the conditions necessary

--- PROCEDURES/PROCEDURE.sql
+++ PROCEDURES/PROCEDURE.sql
@@ -67,46 +67,99 @@ $$;
 -- END;
 -- $$;
 
-CREATE OR REPLACE PROCEDURE update_total_amount()
-    RETURNS STRING
-    LANGUAGE SQL
-    AS
-$$
+-- CREATE OR REPLACE PROCEDURE update_total_amount()
+--     RETURNS STRING
+--     LANGUAGE SQL
+--     AS
+-- $$
+-- BEGIN
+--     -- Update order_header discount based on promotion
+--     UPDATE ORDER_BASE.ORDER_HEADER
+--     SET DISCOUNT = 
+--     (CASE
+--         WHEN oh.TOTAL_AMOUNT < 1000 
+--              AND p.CUSTOMER_CATEGORY IN ('Gold', 'Silver', 'Bronze') 
+--              AND c.CUSTOMER_ID = oh.CUSTOMER_ID 
+--              AND p.CUSTOMER_CATEGORY = c.CATEGORY 
+--                 AND oh.COUPON_CODE = 'Discount'
+--                    AND p.PROMOTION_TYPE = oh.COUPON_CODE
+--                 THEN p.PROMOTION_VALUE * oh.TOTAL_AMOUNT
+--         WHEN oh.COUPON_CODE IN ('Coupon_x', 'Coupon_y', 'Coupon_z') 
+--              AND oh.CUSTOMER_ID = c.CUSTOMER_ID 
+--                 AND oh.COUPON_CODE = 'Discount'
+--                 THEN oh.TOTAL_AMOUNT - (oh.TOTAL_AMOUNT * p.PROMOTION_VALUE)
+        
+--         ELSE 0
+--         END)
+--     FROM ORDER_BASE.ORDER_HEADER oh
+--     JOIN ORDER_BASE.CUSTOMER c ON oh.CUSTOMER_ID = c.CUSTOMER_ID
+--     JOIN ORDER_BASE.PROMOTION p ON p.CUSTOMER_CATEGORY = c.CATEGORY
+--     WHERE oh.CUSTOMER_ID = c.CUSTOMER_ID;
+
+--     UPDATE ORDER_BASE.CUSTOMER c
+--     SET LOYALTY_POINTS = p.PROMOTION_VALUE + LOYALTY_POINTS
+--     FROM ORDER_BASE.PROMOTION p
+--     WHERE c.CUSTOMER_ID IN (
+--     SELECT oh.CUSTOMER_ID
+--     FROM ORDER_BASE.ORDER_HEADER oh
+--     WHERE oh.TOTAL_AMOUNT > 1000
+--         AND p.CUSTOMER_CATEGORY IN ('Gold', 'Silver', 'Bronze') 
+--         AND oh.COUPON_CODE = 'Loyalty'
+--         AND p.PROMOTION_TYPE = oh.COUPON_CODE
+
+--     CALL update_order_amounts();
+-- -     RETURN 'Total amounts updated successfully';
+-- END;
+-- $$;
+
+CREATE OR REPLACE PROCEDURE INTEL_ASSESMENT.ORDER_BASE.UPDATE_TOTAL_AMOUNT()
+RETURNS VARCHAR(16777216)
+LANGUAGE SQL
+EXECUTE AS OWNER
+AS '
 BEGIN
     -- Update order_header discount based on promotion
-    UPDATE ORDER_BASE.ORDER_HEADER
+    UPDATE ORDER_BASE.ORDER_HEADER oh
     SET DISCOUNT = 
     (CASE
-        WHEN oh.TOTAL_AMOUNT < 1000 
-             AND p.CUSTOMER_CATEGORY IN ('Gold', 'Silver', 'Bronze') 
+        WHEN  oh.COUPON_CODE = ''Discount''
+            -- AND oh.TOTAL_AMOUNT < 1000 
+             AND p.CUSTOMER_CATEGORY IN (''Gold'', ''Silver'', ''Bronze'') 
              AND c.CUSTOMER_ID = oh.CUSTOMER_ID 
-             AND p.CUSTOMER_CATEGORY = c.CATEGORY 
-                AND oh.COUPON_CODE = 'Discount'
-                   AND p.PROMOTION_TYPE = oh.COUPON_CODE
+             -- AND p.CUSTOMER_CATEGORY = c.CATEGORY 
+             AND oh.COUPON_CODE = ''Discount''
+             AND p.PROMOTION_TYPE = oh.COUPON_CODE
                 THEN p.PROMOTION_VALUE * oh.TOTAL_AMOUNT
-        WHEN oh.COUPON_CODE IN ('Coupon_x', 'Coupon_y', 'Coupon_z') 
+        WHEN oh.COUPON_CODE IN (''Coupon_x'', ''Coupon_y'', ''Coupon_z'') 
              AND oh.CUSTOMER_ID = c.CUSTOMER_ID 
-                AND oh.COUPON_CODE = 'Discount'
+               AND p.CUSTOMER_CATEGORY IN (''Regular'')
+                -- AND oh.TOTAL_AMOUNT < 1000
                 THEN oh.TOTAL_AMOUNT - (oh.TOTAL_AMOUNT * p.PROMOTION_VALUE)
         
         ELSE 0
         END)
-    FROM ORDER_BASE.ORDER_HEADER oh
-    JOIN ORDER_BASE.CUSTOMER c ON oh.CUSTOMER_ID = c.CUSTOMER_ID
+    
+    from ORDER_BASE.CUSTOMER c 
     JOIN ORDER_BASE.PROMOTION p ON p.CUSTOMER_CATEGORY = c.CATEGORY
-    WHERE oh.CUSTOMER_ID = c.CUSTOMER_ID;
+    WHERE oh.CUSTOMER_ID = c.CUSTOMER_ID ;
 
     UPDATE ORDER_BASE.CUSTOMER c
     SET LOYALTY_POINTS = p.PROMOTION_VALUE + LOYALTY_POINTS
     FROM ORDER_BASE.PROMOTION p
+    -- JOIN ORDER_BASE.ORDER_HEADER oh ON p.CUSTOMER_CATEGORY = c.CATEGORY
     WHERE c.CUSTOMER_ID IN (
     SELECT oh.CUSTOMER_ID
     FROM ORDER_BASE.ORDER_HEADER oh
-    WHERE oh.TOTAL_AMOUNT > 1
-        AND p.CUSTOMER_CATEGORY IN ('Gold', 'Silver', 'Bronze') 
-        AND oh.COUPON_CODE = 'Loyalty'
+    WHERE oh.TOTAL_AMOUNT > 1000
+        AND p.CUSTOMER_CATEGORY IN (''Gold'', ''Silver'', ''Bronze'') 
+        AND oh.COUPON_CODE = ''Loyalty''
         AND p.PROMOTION_TYPE = oh.COUPON_CODE
+);
+
 
     CALL update_order_amounts();
-);
+
+    RETURN ''Total amounts updated successfully'';
+END;
+';
 
